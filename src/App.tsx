f9ba546e8a031816700f2435719e1899bf15342c
obrunofontana <obrunofontana@gmail.com>
2021-12-07 11:43:18 -0300

    chore: Atualiza eslint e configura eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,40 @@
-import React, { lazy, Suspense } from "react";
-import GlobalStyle from "./globalStyle";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import React, { lazy, Suspense } from 'react';
+import {
+  BrowserRouter as Router, Routes, Route, Link,
+} from 'react-router-dom';
+import GlobalStyle from './globalStyle';
 
 // Só irá carregar a página quando realmente for requisitada
-const About = lazy(() => import("./About"));
-const Home = lazy(() => import("./Home"));
+const About = lazy(() => import('./About'));
+const Home = lazy(() => import('./Home'));
 
 // import { Container } from './styles';
 
-const App: React.FC = () => (
-  <>
-    <GlobalStyle />
-    <Router>
-      <Suspense fallback={<div>Carregando...</div>}>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
+const App: React.FC = function () {
+  return (
+    <>
+      <GlobalStyle />
+      <Router>
+        <Suspense fallback={<div>Carregando...</div>}>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+          </nav>
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </Suspense>
-    </Router>
-  </>
-);
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </Suspense>
+      </Router>
+    </>
+  );
+};
 
 export default App;
